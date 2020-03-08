fde75dee7a10ae4019c4d828463cb8a4f81ad08d
Alex-1983 <1983-1111@mail.ru>
2020-03-08 22:57:15 +0500

    final

---
+++ source/js/smooth-scroll.js
@@ -0,0 +1,45 @@
+'use strict';
+
+(function() {
+  var sections = document.querySelectorAll('[data-block]');
+
+  if (!sections) {
+    return;
+  }
+
+  var halfHeightWindow = window.innerHeight / 2;
+
+  var getPosition = function(blocks) {
+    var position = [];
+    for (var i = 0; i < blocks.length; i++) {
+      position[i] = {
+        name: blocks[i].dataset.block,
+        position: blocks[i].getBoundingClientRect().top,
+      };
+    }
+    return position;
+  };
+
+  var checkPosition = function(blocks) {
+    var block = false;
+    for (var i = 0; i < blocks.length; i++) {
+      if (blocks[i].position >= 0 && blocks[i].position <= halfHeightWindow) {
+        block = blocks[i].name;
+      }
+    }
+    return block;
+  };
+
+  var sectionPositon = getPosition(sections);
+  var activeBlock = checkPosition(sectionPositon);
+
+  var onScroll = function() {
+    sectionPositon = getPosition(sections);
+    if (activeBlock !== checkPosition(sectionPositon) && checkPosition(sectionPositon)) {
+      activeBlock = checkPosition(sectionPositon);
+
+    }
+  };
+
+  window.addEventListener('scroll', onScroll);
+})();
